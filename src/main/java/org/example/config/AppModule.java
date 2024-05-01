48b37721957466ac3906e001cbafcdb2fddf81dc
Hichem Touati <hichemtouati@outlook.com>
2024-05-02 01:08:54 +0200

    Added gson for DI

--- src/main/java/org/example/config/AppModule.java
+++ src/main/java/org/example/config/AppModule.java
@@ -1,4 +1,15 @@
 package org.example.config;
 
-public class AppModule {
+import com.google.gson.Gson;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+public class AppModule extends AbstractModule {
+
+    @Provides
+    @Singleton
+    public Gson provideGson() {
+        return new Gson();
+    }
 }
