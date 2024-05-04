09add35ded9d592e4ed3f0a248373bad3781b4a7
Hichem Touati <hichemtouati@outlook.com>
2024-05-04 04:29:45 +0200

    created bindings

--- src/main/java/org/example/config/AppModule.java
+++ src/main/java/org/example/config/AppModule.java
@@ -4,8 +4,19 @@ import com.google.gson.Gson;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
+import org.example.finance.BalanceManager;
+import org.example.finance.TransactionProcessor;
+import org.example.finance.interfaces.IBalanceManager;
+import org.example.finance.interfaces.ITransactionProcessor;
 
 public class AppModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(ITransactionProcessor.class).to(TransactionProcessor.class).in(Singleton.class);
+        bind(IBalanceManager.class).to(BalanceManager.class).in(Singleton.class);
+        // If TransactionList requires binding, add it here. Otherwise, it's not necessary.
+    }
 
     @Provides
     @Singleton
