e05bff75e90646966ebf8d3b545591c0948f9d72
Hichem Touati <hichemtouati@outlook.com>
2024-05-04 04:32:20 +0200

    Under cunstruction

--- src/main/java/org/example/finance/TransactionProcessor.java
+++ src/main/java/org/example/finance/TransactionProcessor.java
@@ -1,45 +1,30 @@
 package org.example.finance;
 
 import org.example.finance.interfaces.ITransactionProcessor;
-import org.example.model.Transaction;
-import org.example.model.TransactionList;
-import java.math.BigDecimal;
 
 public class TransactionProcessor implements ITransactionProcessor {
-    private FinancialAccount financialAccount;
-
-    // Constructor injection for FinancialAccount
-    public TransactionProcessor(FinancialAccount financialAccount) {
-        this.financialAccount = financialAccount;
-    }
 
     @Override
-    public void processIncome(Transaction transaction) {
-        // Implementation for processing income transactions
-        financialAccount.depositToAccountBalance(transaction.getAmount());
+    public void processTransaction(Transaction transaction) {
+        switch (transaction.getType()) {
+            case INCOME:
+                financialAccount.depositToAccountBalance(transaction.getAmount());
+                break;
+            case EXPENSE:
+                financialAccount.withdrawFromAccountBalance(transaction.getAmount());
+                break;
+            case SAVINGS_DEPOSIT:
+                financialAccount.depositToSavingsBalance(transaction.getAmount());
+                break;
+            case SAVINGS_WITHDRAWAL:
+                financialAccount.withdrawFromSavingsBalance(transaction.getAmount());
+                break;
+            case DEBT_PAYMENT:
+                financialAccount.payDebt(transaction.getAmount());
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported transaction type: " + transaction.getType());
+        }
     }
-
-    @Override
-    public void processExpense(Transaction transaction) {
-        // Implementation for processing expense transactions
-        financialAccount.withdrawFromAccountBalance(transaction.getAmount());
-    }
-
-    @Override
-    public void processSavingsDeposit(Transaction transaction) {
-        // Implementation for processing savings deposit transactions
-        financialAccount.depositToSavingsBalance(transaction.getAmount());
-    }
-
-    @Override
-    public void processSavingsWithdrawal(Transaction transaction) {
-        // Implementation for processing savings withdrawal transactions
-        financialAccount.withdrawFromSavingsBalance(transaction.getAmount());
-    }
-
-    @Override
-    public void processDebtPayment(Transaction transaction) {
-        // Implementation for processing debt payment transactions
-        financialAccount.payDebt(transaction.getAmount());
-    }
+}
 }
